693646b03b494cd6529314f434e3802e025b4dcc
Will Hawkins <willhawkins123@yahoo.com>
2017-02-23 17:29:26 -0500

    Can Edit and Delete Individual Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,25 @@ class ItemsController < ApplicationController
     redirect_to "/patrons/#{@patron.id}"
   end
 
+  def edit_item
+    @item = Item.find(params[:id])
+  end
+
+  def update_item
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to "/patrons/#{@item.patron.id}"
+  end
+
+  def destroy_item
+    @item = Item.find(params[:id])
+    @item.destroy
+
+      redirect_to "/patrons/#{@item.patron.id}"
+
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :price, :modifications, :modifications_price, :patron_id)
