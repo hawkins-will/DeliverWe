e479418acdf148dc349e01ef839c04ac3bb31703
Will Hawkins <willhawkins123@yahoo.com>
2017-02-27 11:33:44 -0500

    Can Make Restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,10 +9,20 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+
+    redirect_to root_path
+  end
+
 
   private
   def restaurant_params
-    params.require(:restaurant).permit(:name, :logo_url, :menu_link, :delivery_fee, :delivery_min)
+    params.require(:restaurant).permit(:name, :logo_url, :menu_link, :delivery_fee, :delivery_min, :tax)
   end
 
 end
